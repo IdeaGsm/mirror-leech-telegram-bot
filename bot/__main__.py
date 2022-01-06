ff1506bdb6239a36f6ebdade28b11613ad040a40
IdeaGsm <97039409+IdeaGsm@users.noreply.github.com>
2022-01-07 03:50:47 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -62,8 +62,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+MwgSi5vmQEA2N2Vk")
+    buttons.buildbutton("Super..!", " https://t.me/t4ugdrive_bot")
+    buttons.buildbutton("Cool..!", "https://t.me/n4gdrive_bot")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
